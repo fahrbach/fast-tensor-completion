f23014c15c976f85aadd55d15fff99057be3b297
Matthew Fahrbach <matthew.fahrbach@gmail.com>
2025-01-30 03:12:52 +0000

    Misc updates

--- main.py
+++ main.py
@@ -8,17 +8,14 @@ import tensorly as tl
 
 
 def run_cp_completion_sweep(X, output_path):
-    #seeds = [0, 1, 2, 3, 4, 5]
     seeds = [0]
     sample_ratios = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10]
-    #sample_ratios = [0.05, 0.10, 0.15, 0.20, 0.25]
     ranks = [1, 2, 4, 8, 16]
     for seed in seeds:
         for sample_ratio in sample_ratios:
             for rank in ranks:
                 print('Running... (seed, sample_ratio, rank):', (seed, sample_ratio, rank))
                 result = run_cp_completion(X, sample_ratio, rank, output_path, seed=seed)
-    #return
  
     # Second plot
     sample_ratios = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
@@ -31,8 +28,6 @@ def run_cp_completion_sweep(X, output_path):
 
 
 def run_parafac_als_sweep(X, output_path):
-    #seeds = [0, 1, 2, 3, 4, 5]
-    seeds = [0]
     # sample_ratios = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10]
     # #sample_ratios = [0.05, 0.10, 0.15, 0.20, 0.25]
     # ranks = [1, 2, 4, 8, 16]
@@ -44,6 +39,8 @@ def run_parafac_als_sweep(X, output_path):
     # return
     
     # Second plot
+    #seeds = [0]
+    seeds = [0, 1, 2, 3, 4]
     sample_ratios = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
     ranks = [16]
     for seed in seeds:
@@ -54,12 +51,9 @@ def run_parafac_als_sweep(X, output_path):
 
 
 def run_lifted_cp_completion_sweep(X, output_path, use_acceleration=False):
-    #seeds = [0, 1, 2, 3, 4, 5]
-    seeds = [0]
+    #seeds = [0]
+    seeds = [0, 1, 2, 3, 4]
 
-    #sample_ratios = [0.10]
-    #sample_ratios = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10]
-    #sample_ratios = [0.05, 0.10, 0.15, 0.20, 0.25]
     sample_ratios = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
     epsilons = [0.1, 0.01, 0.001, 0.0001]
     rank = 16
@@ -73,6 +67,7 @@ def run_lifted_cp_completion_sweep(X, output_path, use_acceleration=False):
 def main():
     colors = mpl.colormaps['tab10'].colors
 
+    """
     data_manager = TensorDataManager()
     X = data_manager.generate_random_normal(shape=(50, 50, 50))
     output_path = data_manager.output_path
@@ -82,6 +77,7 @@ def main():
     run_lifted_cp_completion_sweep(X, output_path)
     run_lifted_cp_completion_sweep(X, output_path, use_acceleration=True)
     return
+    """
 
     data_manager = TensorDataManager()
     X = data_manager.generate_random_normal(shape=(100, 100, 100))
@@ -97,6 +93,7 @@ def main():
     output_path = data_manager.output_path
     print(data_manager.output_path)
     run_cp_completion_sweep(X, output_path)
+    run_parafac_als_sweep(X, output_path)
     run_lifted_cp_completion_sweep(X, output_path)
     run_lifted_cp_completion_sweep(X, output_path, use_acceleration=True)
 
@@ -105,6 +102,7 @@ def main():
     output_path = data_manager.output_path
     print(data_manager.output_path)
     run_cp_completion_sweep(X, output_path)
+    run_parafac_als_sweep(X, output_path)
     run_lifted_cp_completion_sweep(X, output_path)
     run_lifted_cp_completion_sweep(X, output_path, use_acceleration=True)
 
@@ -113,6 +111,7 @@ def main():
     output_path = data_manager.output_path
     print(data_manager.output_path)
     run_cp_completion_sweep(X, output_path)
+    run_parafac_als_sweep(X, output_path)
     run_lifted_cp_completion_sweep(X, output_path)
     run_lifted_cp_completion_sweep(X, output_path, use_acceleration=True)
 
@@ -122,24 +121,19 @@ def main():
     output_path = data_manager.output_path
     print(data_manager.output_path)
     run_cp_completion_sweep(X, output_path)
+    run_parafac_als_sweep(X, output_path)
     run_lifted_cp_completion_sweep(X, output_path)
     run_lifted_cp_completion_sweep(X, output_path, use_acceleration=True)
 
     return
 
-    # Traffic
-    data_manager = TensorDataManager()
-    X = data_manager.load_traffic()
-    output_path = data_manager.output_path
-    print(data_manager.output_path)
-    run_cp_completion_sweep(X, output_path)
-    run_lifted_cp_completion_sweep(X, output_path)
-    return
+
+
 
 
     data_manager = TensorDataManager()
     #X = data_manager.generate_random_normal(shape=(50, 50, 50))
-    X = data_manager.generate_random_normal(shape=(100, 100, 100))
+    #X = data_manager.generate_random_normal(shape=(100, 100, 100))
 
     #X = data_manager.generate_random_cp(shape=(50, 50, 50), rank=16)
     #X = data_manager.generate_random_cp(shape=(100, 100, 100), rank=16)
@@ -149,7 +143,7 @@ def main():
     #X = data_manager.generate_random_tucker(shape=(100, 100, 100), rank=(4, 4, 4))
 
     #X = data_manager.load_cardiac_mri()
-    #X = data_manager.load_hyperspectral()
+    X = data_manager.load_hyperspectral()
 
     output_path = data_manager.output_path
     print('X.shape:', X.shape)
